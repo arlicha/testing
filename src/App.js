f0b5a6824e7cd9132c217759bdb47512be26ceea
arlicha <lichachowdhury@gmail.com>
2022-05-10 23:59:12 +0600

    hearder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Hearder from './component/Heardet/Hearder';
 
 function App() {
   return (
     <div className="App">
       
       <h1>hlw</h1>
+      <Hearder></Hearder>
     </div>
   );
 }
---
+++ src/component/Heardet/Hearder.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hearder = () => {
+    return (
+        <div>
+            <h1>this is hearder</h1>
+        </div>
+    );
+};
+
+export default Hearder;
